814687f7dd7e26d203fa2b198be439975b7f4a6c
Manuel <manuel@empacotecnic.com>
2021-04-14 05:08:24 -0500

    fullback4

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,21 +1,21 @@
-import {createClient} from 'contentful'
-import Image from 'next/image'
+import { createClient } from 'contentful'
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+import Image from 'next/image'
 import Skeleton from '../../components/Skeleton'
 
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
-  accessToken: process.env.CONTENFUL_ACCESS_KEY,
+  accessToken: process.env.CONTENTFUL_ACCESS_KEY,
 })
 
 export const getStaticPaths = async () => {
-  const res = await client.getEntries({
-    content_type: 'recipe' 
-  }) 
+  const res = await client.getEntries({ 
+    content_type: "recipe" 
+  })
 
   const paths = res.items.map(item => {
     return {
-      params: {slug: item.fields.slug}
+      params: { slug: item.fields.slug }
     }
   })
 
@@ -25,8 +25,7 @@ export const getStaticPaths = async () => {
   }
 }
 
-export async function getStaticProps({ params }){
-
+export const getStaticProps = async ({ params }) => {
   const { items } = await client.getEntries({
     content_type: 'recipe',
     'fields.slug': params.slug
@@ -36,37 +35,39 @@ export async function getStaticProps({ params }){
     props: { recipe: items[0] },
     revalidate: 1
   }
-
 }
 
 export default function RecipeDetails({ recipe }) {
-  if(!recipe) return <Skeleton />
+  if (!recipe) return <Skeleton />
 
   const { featuredImage, title, cookingTime, ingredients, method } = recipe.fields
 
   return (
-    <Sk>
+    <div>
       <div className="banner">
-        <Image
-          src= { 'https:' + featuredImage.fields.file.url}
-          width= {featuredImage.fields.file.details.image.width}
-          height= {featuredImage.fields.file.details.image.height}
+        <Image 
+          src={'https:' + featuredImage.fields.file.url}
+          width={featuredImage.fields.file.details.image.width}
+          height={featuredImage.fields.file.details.image.height}
         />
-        <h2>{title}</h2>
+        <h2>{ title }</h2>
       </div>
+
       <div className="info">
-        <p>Takes {cookingTime} mins to make</p>
-        <h3>Ingedients:</h3>
+        <p>Takes about { cookingTime } mins to cook.</p>
+        <h3>Ingredients:</h3>
+
         {ingredients.map(ing => (
-          <span key={ing}>{ing}</span>
+          <span key={ing}>{ ing }</span>
         ))}
       </div>
+        
       <div className="method">
         <h3>Method:</h3>
         <div>{documentToReactComponents(method)}</div>
       </div>
-      <style jsx>
-        {`
+
+      <style jsx>{`
         h2,h3 {
           text-transform: uppercase;
         }
@@ -90,9 +91,7 @@ export default function RecipeDetails({ recipe }) {
         .info span:last-child::after {
           content: ".";
         }
-      `}
-      </style>
-    </Sk>
+      `}</style>
+    </div>
   )
 }
-
